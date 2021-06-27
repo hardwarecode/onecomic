3fcf4fde51e16469a8b2f268dee84d29afd9ba2f
hardwarecode <hardwarecode>
2021-06-27 11:19:33 +0800

    requests改用httpx

--- onecomic/proxy/kuaidaili.py
+++ onecomic/proxy/kuaidaili.py
@@ -1,16 +1,25 @@
-import requests
+import httpx
 from . import ProxyBase
+
+SESSION = None
+
+
+def get_session():
+    global SESSION
+    if SESSION is None:
+        SESSION = httpx.Client(verify=False)
+    return SESSION
 
 
 class KuaidailiProxy(ProxyBase):
     NAME = 'kuaidaili'
     API_URL = None
-    session = requests.Session()
 
     @classmethod
     def get_proxy(cls):
         assert cls.API_URL, 'KuaidailiProxy API_URL not set'
-        proxy_ip = cls.session.get(cls.API_URL).text
+        session = get_session()
+        proxy_ip = session.get(cls.API_URL).text
         return "socks5://%(proxy)s/" % {"proxy": proxy_ip}
 
     @classmethod
