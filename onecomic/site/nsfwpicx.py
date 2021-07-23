cf4bc02d79d233c92e1560f0b27f49d23ace6162
hardwarecode <hardwarecode>
2021-07-24 00:40:50 +0800

    add nsfwpicx

--- onecomic/site/nsfwpicx.py
+++ onecomic/site/nsfwpicx.py
@@ -7,20 +7,20 @@ from ..crawlerbase import CrawlerBase
 logger = logging.getLogger(__name__)
 
 
-class PicxxxxCrawler(CrawlerBase):
+class NsfwpicxCrawler(CrawlerBase):
 
-    SITE = "picxxxx"
-    SITE_INDEX = 'http://picxxxx.top/'
+    SITE = "nsfwpicx"
+    SITE_INDEX = 'http://kkoo.icu/'
     SOURCE_NAME = "Nsfwpicx"
     LOGIN_URL = SITE_INDEX
     R18 = True
 
-    DEFAULT_COMICID = '2020-12-08-2750'
+    DEFAULT_COMICID = '1802'
     DEFAULT_SEARCH_NAME = ''
     DEFAULT_TAG = ""
-    COMICID_PATTERN = re.compile(r'picxxxx\.top/(\d{4}[\/\-]\d{2}[\/\-]\d{2}[\/\-]\d+)(?:\.html)?')
+    COMICID_PATTERN = re.compile(r'kkoo\.icu/(\d+)\.html')
     SINGLE_CHAPTER = True
-    SITE_ENABLE = False
+    SITE_ENABLE = True
 
     @classmethod
     def get_comicid_by_url(cls, comicid_or_url):
@@ -35,15 +35,15 @@ class PicxxxxCrawler(CrawlerBase):
         return self.get_source_url(self.comicid.replace('-', '/'))
 
     def get_source_url(self, comicid):
-        return urljoin(self.SITE_INDEX, 'http://picxxxx.top/%s.html' % comicid)
+        return urljoin(self.SITE_INDEX, 'http://kkoo.icu/%s.html' % comicid)
 
     def get_comicbook_item(self):
         soup = self.get_soup(self.source_url)
         name = self.comicid
         author = ''
         desc = ''
-        image_urls = [img.get('data-src') for img in
-                      soup.find('div', {'itemprop': 'articleBody'}).find_all('img')]
+        image_urls = [img.get('data-src') or img.get('src') for img in
+                      soup.find('div', {'class': 'entry-content'}).find_all('img')]
         book = self.new_comicbook_item(name=name,
                                        desc=desc,
                                        cover_image_url=image_urls[0],
@@ -53,8 +53,8 @@ class PicxxxxCrawler(CrawlerBase):
                          image_urls=image_urls)
         return book
 
-    def get_chapter_item(self, citem):
-        return citem
+    def get_chapter_image_urls(self, citem):
+        return citem.image_urls
 
     def latest(self, page=1):
         if page > 1:
@@ -63,14 +63,42 @@ class PicxxxxCrawler(CrawlerBase):
             url = self.SITE_INDEX
         soup = self.get_soup(url)
         result = self.new_search_result_item()
-        for li in soup.find('ul', {'id': 'masonry'}).find_all('li'):
-            href = li.a.get('href')
+        for a in soup.find_all('a', {'class': 'entry-image'}):
+            href = a.get('href')
             source_url = urljoin(self.SITE_INDEX, href)
             comicid = self.get_comicid_by_url(source_url)
             name = comicid
-            cover_image_url = ''
-            if li.img:
-                cover_image_url = li.img.get('src')
+            cover_image_url = a.img.get('src')
+            result.add_result(comicid=comicid,
+                              name=name,
+                              cover_image_url=cover_image_url,
+                              source_url=source_url)
+        return result
+
+    def get_tags(self):
+        tags = self.new_tags_item()
+        for tag, name in [
+            ('asia', 'ASIA'),
+            ('usa', 'USA'),
+            ('cosplay', 'COSPLAY'),
+            ('random', 'RANDOM'),
+        ]:
+            tags.add_tag(category='分类', tag=tag, name=name)
+        return tags
+
+    def get_tag_result(self, tag, page):
+        if page > 1:
+            url = urljoin(self.SITE_INDEX, "/category/%s/page/%s" % (tag, page))
+        else:
+            url = urljoin(self.SITE_INDEX, "/category/%s" % tag)
+        soup = self.get_soup(url)
+        result = self.new_search_result_item()
+        for a in soup.find_all('a', {'class': 'entry-image'}):
+            href = a.get('href')
+            source_url = urljoin(self.SITE_INDEX, href)
+            comicid = self.get_comicid_by_url(source_url)
+            name = comicid
+            cover_image_url = a.img.get('src')
             result.add_result(comicid=comicid,
                               name=name,
                               cover_image_url=cover_image_url,
