89b936e53b4dd8a8a4f549f04642e7c36abc78d2
hardwarecode <hardwarecode>
2022-05-29 21:16:08 +0800

    修复酷爱漫画图片抓取

--- onecomic/site/kuimh.py
+++ onecomic/site/kuimh.py
@@ -70,7 +70,7 @@ class KuimhCrawler(CrawlerBase):
         soup = self.get_soup(citem.source_url)
         image_urls = []
         for i in soup.find('div', {'class': 'comicpage'}).find_all('div', recursive=False):
-            image_url = i.img.get('data-echo') or i.get('src')
+            image_url = i.img.get('data-echo') or i.img.get('src')
             image_urls.append(image_url)
         return image_urls
 
