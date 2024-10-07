403f4006cfe2f573101eb79091bc66f7a028e0ee
hardwarecode <hardwarecode>
2024-10-07 21:27:17 +0800

    更新拷贝漫画

--- onecomic/site/copymanga.py
+++ onecomic/site/copymanga.py
@@ -1,16 +1,38 @@
 import re
+import binascii
 import logging
+import json
 from urllib.parse import urljoin
-
+from Cryptodome.Cipher import AES
 from ..crawlerbase import CrawlerBase
 
 logger = logging.getLogger(__name__)
 
 
+KEY = b"xxxmanga.woo.key"
+
+
+def add_to_16(value):
+    while len(value) % 16 != 0:
+        value += b'\x10'
+    return value
+
+
+def aes_encrypt(key, t, iv):
+    aes = AES.new(add_to_16(key), AES.MODE_CBC, add_to_16(iv))
+    return aes.encrypt(add_to_16(t))
+
+
+def aes_decrypt(key, t, iv):
+    aes = AES.new(add_to_16(key), AES.MODE_CBC, add_to_16(iv))
+    return aes.decrypt(t).rstrip(b'\x10')
+
+
+
 class CopymangaCrawler(CrawlerBase):
 
     SITE = "copymanga"
-    SITE_INDEX = 'https://copymanga.com/'
+    SITE_INDEX = 'https://copymanga.tv/'
     SOURCE_NAME = "拷贝漫画"
     LOGIN_URL = SITE_INDEX
     COMICID_PATTERN = re.compile(r'/comic/([_a-zA-Z0-9\-]*)/?')
@@ -28,19 +50,12 @@ class CopymangaCrawler(CrawlerBase):
 
     def get_chapters(self, comicid):
         chapters = []
-        offset = 0
-        limit = 500
-        api_url = 'https://api.copymanga.com/api/v3/comic/%s/group/default/chapters?limit=%s&offset=%s'
-        url = api_url % (comicid, limit, offset)
-        data = self.get_json(url)
-        total = data['results']['total']
-        chapters.extend(data['results']['list'])
-        for i in range(int(total / limit)):
-            offset += limit
-            url = api_url % (comicid, limit, offset)
-            data = self.get_json(url)
-            chapters.extend(data['results']['list'])
-        return chapters
+        api_url = urljoin(self.SITE_INDEX, "/comicdetail/{}/chapters".format(comicid))
+        data = self.get_json(api_url)
+        results = data['results']
+        s = aes_decrypt(KEY, binascii.a2b_hex(results[16:]), results[:16].encode())
+        res = json.loads(re.sub("[\x00-\x08\x0b\x0c\x0e-\x1f\x7f-\xff]", "", s.decode("utf-8")))
+        return res['groups']['default']['chapters']
 
     def get_comicbook_item(self):
         soup = self.get_soup(self.source_url)
@@ -72,7 +87,7 @@ class CopymangaCrawler(CrawlerBase):
         for tag_id, tag_name in tag_list:
             book.add_tag(name=tag_name, tag=tag_id)
         for chapter_number, item in enumerate(self.get_chapters(self.comicid), start=1):
-            uuid = item['uuid']
+            uuid = item['id']
             url = urljoin(self.SITE_INDEX, "/comic/%s/chapter/%s" % (self.comicid, uuid))
             title = item['name']
             book.add_chapter(chapter_number=chapter_number,
@@ -82,20 +97,23 @@ class CopymangaCrawler(CrawlerBase):
         return book
 
     def get_chapter_image_urls(self, citem):
-        api_url = "https://api.copymanga.com/api/v3/comic/%s/chapter/%s?platform=1&_update=true"
-        url = api_url % (self.comicid, citem.uuid)
-        data = self.get_json(url)
-        image_urls = [i['url'] for i in data['results']['chapter']['contents']]
+        html = self.get_html(citem.source_url)
+        r = re.search('class="imageData" contentKey="(.*?)"', html)
+        contentKey = r.group(1)
+        s = aes_decrypt(KEY, binascii.a2b_hex(contentKey[16:]), contentKey[:16].encode())
+        res = json.loads(re.sub("[\x00-\x08\x0b\x0c\x0e-\x1f\x7f-\xff]", "", s.decode("utf-8")))
+        image_urls = [i['url'] for i in res]
         return image_urls
 
     def get_chapter_from_page(self, soup):
         result = self.new_search_result_item()
-        for div in soup.find('div', {'class': 'row exemptComic-box'}).find_all('div', recursive=False):
-            href = div.a.get('href')
-            name = div.find('p', {'class': 'twoLines'}).text
-            comicid = self.get_comicid_by_url(href)
-            source_url = urljoin(self.SITE_INDEX, href)
-            cover_image_url = div.img.get('data-src')
+        data_list_str = soup.find('div', {'class': 'row exemptComic-box'}).get('list')
+        data_list_str = data_list_str.replace("&#x27;", '"')
+        for item in eval(data_list_str):
+            comicid = item['path_word']
+            name = item['name']
+            cover_image_url = item['cover']
+            source_url = self.get_source_url(comicid)
             result.add_result(comicid=comicid,
                               name=name,
                               cover_image_url=cover_image_url,
@@ -134,8 +152,9 @@ class CopymangaCrawler(CrawlerBase):
         offset = (page - 1) * limit
         url = urljoin(
             self.SITE_INDEX,
-            "/api/kb/web/searchs/comics?offset=%s&platform=2&limit=%s&q=%s&q_type=" % (offset, limit, name)
+            "/api/kb/web/searchbc/comics?offset=%s&platform=2&limit=%s&q=%s&q_type=" % (offset, limit, name)
         )
+
 
         data = self.get_json(url)
         result = self.new_search_result_item()
